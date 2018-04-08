1161c4fa4927188b3ce7a25de367afa35adbe6bb
Ali_Baheri <ali.baheri4614@gmail.com>
2018-04-08 10:26:00 -0400

    v3

--- model.py
+++ model.py
@@ -1,186 +1,159 @@
-import numpy as np
-import pandas as pd
+# sklearn libs
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+# keras libs
+from keras import backend
 from keras.models import Sequential
-from keras.layers import Convolution2D, ELU, Flatten, Dropout, Dense, Lambda, MaxPooling2D
-from keras.preprocessing.image import img_to_array, load_img
+from keras.layers import Flatten, Dense, Lambda, Dropout
+from keras.layers import Convolution2D, Cropping2D
+# misc. import
+import numpy as np 
+import csv
 import cv2
 
-rows, cols, ch = 64, 64, 3
 
-TARGET_SIZE = (64, 64)
+# tuning parameters 
+steering_correction = 0.15 
+steering_threshold = 0.285  
+batch_size = 64
+num_epoch = 20
 
+# initial lists
+lines = [] 			# for data log
+images = [] 		# for features input
+steering_set = [] 	# for labels output
 
-def augment_brightness_camera_images(image):
-    '''
-    :param image: Input image
-    :return: output image with reduced brightness
-    '''
+def get_log():
+	with open('data/driving_log.csv') as csvfile:
+	  reader = csv.reader(csvfile)
+	  for line in reader:
+	    lines.append(line)
 
-    # convert to HSV so that its easy to adjust brightness
-    image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+	del(lines[0]) 	# remove the first line (string)
+	shuffle(lines) 	# shuffle samples
+	# split into training and validation datasets 80,20.
+	training_set, validation_set = train_test_split(lines, test_size=0.2, random_state=100)
+	return (training_set, validation_set)
 
-    # randomly generate the brightness reduction factor
-    # Add a constant so that it prevents the image from being completely dark
-    random_bright = .25+np.random.uniform()
+def brightness_process(image):
+	image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV) # convert from RGB to HSV color
+	brightness_random =  .23 + np.random.uniform() # random brightness 
+	image[:,:,2] = image[:,:,2]*brightness_random  # add brightness to the image
+	image[:,:,2][image[:,:,2]>255] = 255		   # keep image at 255 if greater than that 
+	image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB) # convert back to RGB from HSV
+	return image
 
-    # Apply the brightness reduction to the V channel
-    image1[:,:,2] = image1[:,:,2]*random_bright
+def shadow_augmentation(image):
+	'''refer to: https://chatbotslife.com/using-augmentation-to-mimic-human-driving-496b569760a9
+	'''
+	height, width, _ = image.shape
+	imgtop_y = width*np.random.uniform() # 320
+	imgtop_x = 0
+	imgbot_y = width*np.random.uniform()
+	imgbot_x = height # 160
+	image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+	shadow_mask = 0*image[:,:,1]
+	mask_X = np.mgrid[0:image.shape[0], 0:image.shape[1]][0]
+	mask_Y = np.mgrid[0:image.shape[0], 0:image.shape[1]][1]
+	shadow_mask[((mask_X - imgtop_x)*(imgbot_y - imgtop_y) - (imgbot_x - imgtop_x)*(mask_Y - imgtop_y) >=0)] = 1
+	if np.random.randint(2) == 1:
+		condition_1 = shadow_mask == 1
+		condition_0 = shadow_mask == 0
+		if np.random.randint(2) == 1:
+			image[:,:,1][condition_1] = image[:,:,1][condition_1]*.5 # random brightness
+		else:
+			image[:,:,1][condition_0] = image[:,:,1][condition_0]*.5 
+	image = cv2.cvtColor(image, cv2.COLOR_HLS2RGB)
+	return image 
 
-    # convert to RBG again
-    image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
-    return image1
+def process_images(center_path, left_path, right_path, steering, images, steering_set):
+	# center
+	center_image = cv2.imread('data/IMG/' + center_path.split('/')[-1]) # extract the image from the path
+	center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)		# convert BGR to RGB
+	center_image = brightness_process(center_image)						# brightness process
+	center_image = shadow_augmentation(center_image)					# shadow augmentation 
+	images.append(center_image)											
+	steering_set.append(steering)
+	# left
+	left_image = cv2.imread('data/IMG/' + left_path.split('/')[-1])
+	left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB)
+	left_image = brightness_process(left_image)
+	left_image = shadow_augmentation(left_image)
+	images.append(left_image)
+	steering_set.append(steering + steering_correction)
+	# right
+	right_image = cv2.imread('data/IMG/' + right_path.split('/')[-1])
+	right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2RGB)
+	right_image = brightness_process(right_image)
+	right_image = shadow_augmentation(right_image)
+	images.append(right_image)
+	steering_set.append(steering - steering_correction)
+	if abs(steering) > steering_threshold:			# if steering is greater than steering_threshold, then flip the image.
+		images.append(cv2.flip(center_image, 1)) 	# flip images (augmentation)
+		steering_set.append(-steering)
+		images.append(cv2.flip(left_image, 1))		
+		steering_set.append(-(steering + steering_correction))
+		images.append(cv2.flip(right_image, 1))		
+		steering_set.append(-(steering - steering_correction))
 
+def generators(datasets, batch_size=batch_size):
+	while True: 
+		shuffle(datasets) # shuffle the datasets
+		for offset in range(0, len(datasets), batch_size):
+			batch_lines = datasets[offset : offset + batch_size]
+			images = []
+			steering_set = []
+			for line in batch_lines:
+				steering = float(line[3]) 
+				process_images(line[0], line[1], line[2], steering, images, steering_set)
+			X_train = np.asarray(images)
+			y_train = np.asarray(steering_set)
+			yield shuffle(X_train, y_train, random_state=100) 
 
-def resize_to_target_size(image):
-    return cv2.resize(image, TARGET_SIZE)
+def nvidia_model():
+	'''Nvidia architectural model with dropout in FCs.'''
+	model = Sequential()
+	model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+	model.add(Cropping2D(cropping=((70,25), (0,0))))  # 70-top, 25-bottom
+	model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+	model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+	model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+	model.add(Convolution2D(64,3,3, activation='relu'))
+	model.add(Convolution2D(64,3,3, activation='relu'))
+	model.add(Flatten())
+	model.add(Dense(100))
+	model.add(Dropout(0.5))
+	model.add(Dense(50))
+	model.add(Dropout(0.5))
+	model.add(Dense(20))
+	model.add(Dropout(0.5))
+	model.add(Dense(10))
+	model.add(Dense(1))
+	model.summary()
+	return model
 
+def run_the_network():
+	# architectural network (nvidia-based)
+	model = nvidia_model()
+	# get data
+	training_set, validation_set = get_log()
+	# train the model and fit generator
+	training_set_generator = generators(training_set, batch_size=batch_size)
+	validation_set_generator = generators(validation_set, batch_size=batch_size)
+	# params for fit generator
+	samples_per_epoch = (3*len(training_set)//batch_size)*batch_size
+	nb_val_samples = len(validation_set)
 
-def crop_and_resize(image):
-    '''
-    :param image: The input image of dimensions 160x320x3
-    :return: Output image of size 64x64x3
-    '''
-    cropped_image = image[55:135, :, :]
-    processed_image = resize_to_target_size(cropped_image)
-    return processed_image
+	model.compile(loss='mse', optimizer='adam')
+	model.fit_generator(training_set_generator, samples_per_epoch= samples_per_epoch, validation_data=validation_set_generator, nb_val_samples=nb_val_samples, nb_epoch=num_epoch, verbose=1)
 
+	# save model
+	model.save('model.h5')
 
-def preprocess_image(image):
-    image = crop_and_resize(image)
-    image = image.astype(np.float32)
+# run the network
+run_the_network()
 
-    #Normalize image
-    image = image/255.0 - 0.5
-    return image
+# avoid 'NonType' object of AttributeError.
+backend.clear_session() 
 
-
-def get_augmented_row(row):
-    steering = row['steering']
-
-    # randomly choose the camera to take the image from
-    camera = np.random.choice(['center', 'left', 'right'])
-
-    # adjust the steering angle for left anf right cameras
-    if camera == 'left':
-        steering += 0.25
-    elif camera == 'right':
-        steering -= 0.25
-
-    image = load_img("udacity_data/" + row[camera].strip())
-    image = img_to_array(image)
-
-    # decide whether to horizontally flip the image:
-    # This is done to reduce the bias for turning left that is present in the training data
-    flip_prob = np.random.random()
-    if flip_prob > 0.5:
-        # flip the image and reverse the steering angle
-        steering = -1*steering
-        image = cv2.flip(image, 1)
-
-    # Apply brightness augmentation
-    image = augment_brightness_camera_images(image)
-
-    # Crop, resize and normalize the image
-    image = preprocess_image(image)
-    return image, steering
-
-
-def get_data_generator(data_frame, batch_size=32):
-    N = data_frame.shape[0]
-    batches_per_epoch = N // batch_size
-
-    i = 0
-    while(True):
-        start = i*batch_size
-        end = start+batch_size - 1
-
-        X_batch = np.zeros((batch_size, 64, 64, 3), dtype=np.float32)
-        y_batch = np.zeros((batch_size,), dtype=np.float32)
-
-        j = 0
-
-        # slice a `batch_size` sized chunk from the dataframe
-        # and generate augmented data for each row in the chunk on the fly
-        for index, row in data_frame.loc[start:end].iterrows():
-            X_batch[j], y_batch[j] = get_augmented_row(row)
-            j += 1
-
-        i += 1
-        if i == batches_per_epoch - 1:
-            # reset the index so that we can cycle over the data_frame again
-            i = 0
-        yield X_batch, y_batch
-
-
-def get_model():
-    model = Sequential()
-    # model.add(Lambda(preprocess_batch, input_shape=(160, 320, 3), output_shape=(64, 64, 3)))
-
-    # layer 1 output shape is 32x32x32
-    model.add(Convolution2D(32, 5, 5, input_shape=(64, 64, 3), subsample=(2, 2), border_mode="same"))
-    model.add(ELU())
-
-    # layer 2 output shape is 15x15x16
-    model.add(Convolution2D(16, 3, 3, subsample=(1, 1), border_mode="valid"))
-    model.add(ELU())
-    model.add(Dropout(.4))
-    model.add(MaxPooling2D((2, 2), border_mode='valid'))
-
-    # layer 3 output shape is 12x12x16
-    model.add(Convolution2D(16, 3, 3, subsample=(1, 1), border_mode="valid"))
-    model.add(ELU())
-    model.add(Dropout(.4))
-
-    # Flatten the output
-    model.add(Flatten())
-
-    # layer 4
-    model.add(Dense(1024))
-    model.add(Dropout(.3))
-    model.add(ELU())
-
-    # layer 5
-    model.add(Dense(512))
-    model.add(ELU())
-
-    # Finally a single output, since this is a regression problem
-    model.add(Dense(1))
-
-    model.compile(optimizer="adam", loss="mse")
-
-    return model
-
-if __name__ == "__main__":
-    BATCH_SIZE = 32
-
-    data_frame = pd.read_csv('udacity_data/driving_log.csv', usecols=[0, 1, 2, 3])
-
-    # shuffle the data
-    data_frame = data_frame.sample(frac=1).reset_index(drop=True)
-
-    # 80-20 training validation split
-    training_split = 0.8
-
-    num_rows_training = int(data_frame.shape[0]*training_split)
-
-    training_data = data_frame.loc[0:num_rows_training-1]
-    validation_data = data_frame.loc[num_rows_training:]
-
-    # release the main data_frame from memory
-    data_frame = None
-
-    training_generator = get_data_generator(training_data, batch_size=BATCH_SIZE)
-    validation_data_generator = get_data_generator(validation_data, batch_size=BATCH_SIZE)
-
-    model = get_model()
-
-    samples_per_epoch = (20000//BATCH_SIZE)*BATCH_SIZE
-
-    model.fit_generator(training_generator, validation_data=validation_data_generator,
-                        samples_per_epoch=samples_per_epoch, nb_epoch=3, nb_val_samples=3000)
-
-    print("Saving model weights and configuration file.")
-
-    model.save_weights('model.h5')  # always save your weights after training or during training
-    with open('model.json', 'w') as outfile:
-        outfile.write(model.to_json())
+### end of code ###
